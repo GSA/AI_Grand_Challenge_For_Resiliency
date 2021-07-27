6865c4ae0edb1c4c13a7d7e210102648a5dd7ef3
avkondepudi <akhil.kondepudi@gsa.gov>
2021-07-27 17:57:05 -0400

    formatted with yapf

--- Data/Utils/parseCFR.py
+++ Data/Utils/parseCFR.py
@@ -6,15 +6,14 @@ import re
 import copy
 import logging
 
+
 class CFR():
     """Class to parse data from CFR XML Title documents.
 
     Attributes:
         _fnames (list[str]) : path to files
     """
-    
-    def __init__(self, 
-        *srcs):
+    def __init__(self, *srcs):
         """Inits attributes.
 
         Args:
@@ -35,17 +34,20 @@ class CFR():
                     self._fnames.append(src)
 
         assert self._fnames
-        
+
     def writeToTextFile(self) -> None:
         """Writes CFR data to text file"""
 
         for fname in self._fnames:
             with open(fname.replace(".xml", ".txt"), "w") as tfile:
                 root = ET.parse(fname).getroot()
-                tfile.write(ET.tostring(root, encoding='utf-8', method='text').decode("utf-8"))
-                
-                logging.debug("Wrote data to text file: {}".format(fname.replace(".xml", ".txt")))
-    
+                tfile.write(
+                    ET.tostring(root, encoding='utf-8',
+                                method='text').decode("utf-8"))
+
+                logging.debug("Wrote data to text file: {}".format(
+                    fname.replace(".xml", ".txt")))
+
     def getData(self) -> (pd.DataFrame, pd.DataFrame):
         """Iterates through XML Element trees to retrieve regulation structure and data
         
@@ -53,54 +55,51 @@ class CFR():
             data (pd.DataFrame): Title information down to the Section level
             parts (pd.DataFrame): Information for all parts in Title, akin to Table of Contents
         """
-        
+
         data = pd.DataFrame(columns=["TITLE", "DATE", "TEXT", "SECTNO"])
         parts = pd.DataFrame()
-        
+
         for fname in self._fnames:
 
             logging.debug("File: {}".format(fname))
             tree = ET.parse(fname)
 
             for title in tree.iter("TITLE"):
-                
+
                 title_name = ""
                 if title.find("CFRTITLE"):
-                    title_name = title.find("CFRTITLE").find("TITLEHD").find("HD").text
+                    title_name = title.find("CFRTITLE").find("TITLEHD").find("HD").text # yapf:disable
                 elif title.find("LRH"):
                     title_name = title.find("LRH").text
-                
+
                 if title_name == "":
-                    logging.warning("Could not find name of title for file: ".format(fname))
-                    
+                    logging.warning("Could not find name of title for file: ".format(fname)) # yapf:disable
+
                 date = [d for d in tree.iter("DATE")][0].text
 
                 for part in title.iter("PART"):
                     if part.tag == "PART":
 
-                        part_info = {
-                            "TITLE": title_name,
-                            "DATE": date
-                        }
-                        
+                        part_info = {"TITLE": title_name, "DATE": date}
+
                         for child in part:
                             if child.tag == "EAR" or child.tag == "RESERVED":
                                 part_info["PART"] = child.text
                                 try:
-                                    part_info["PART NUMBER"] = re.findall("\d+\S*", child.text)[0]
+                                    part_info["PART NUMBER"] = re.findall(r"\d+\S*", child.text)[0] # yapf:disable
                                 except:
                                     part_info["PART NUMBER"] = child.text
-                                    logging.warning("Could not find name of part: ".format(child.text))
+                                    logging.warning("Could not find name of part: ".format(child.text)) # yapf:disable
                             elif child.tag == "HD":
                                 part_info["PART DESC"] = child.text
 
                         parts = parts.append(part_info, ignore_index=True)
 
                         for subpart in part.iter("SUBPART"):
-                            
+
                             sect_info = copy.deepcopy(part_info)
                             sect_info["TEXT"] = ""
-                            
+
                             curr_subpart = ""
                             try:
                                 curr_subpart = subpart.find("HD").text
@@ -108,95 +107,102 @@ class CFR():
                                 # subpart has no information
                                 curr_subpart = subpart.find("RESERVED").text
                                 sect_info["SUBPART"] = curr_subpart
-                                data = data.append(sect_info, ignore_index=True)
-                            
+                                data = data.append(sect_info,
+                                                   ignore_index=True)
+
                             # pull section data
                             for section in subpart.iter("SECTION"):
-                                
+
                                 sect_info = copy.deepcopy(part_info)
                                 sect_info["TEXT"] = ""
-                                    
-                                for child in section: 
-                                    
+
+                                for child in section:
+
                                     # check if section is null
                                     if not child.find("P"): break
 
-                                    if child.tag == "SECTNO": 
-                                        sect_info["SECTNO"] = child.text.split()[-1]
-                                    elif child.tag == "SUBJECT": 
+                                    if child.tag == "SECTNO":
+                                        sect_info["SECTNO"] = child.text.split()[-1] # yapf:disable
+                                    elif child.tag == "SUBJECT":
                                         sect_info["SECT SUBJECT"] = child.text
                                     elif child.tag == "P":
-                                        sect_info["TEXT"] += ''.join(child.itertext()).strip() 
-                                
+                                        sect_info["TEXT"] += ''.join(child.itertext()).strip() # yapf:disable
+
                                 if sect_info["TEXT"] != "":
-                                    data = data.append(sect_info, ignore_index=True)
-                            
+                                    data = data.append(sect_info,
+                                                       ignore_index=True)
+
                             # pull appendix data
                             for appendix in subpart.iter("APPENDIX"):
-                                
+
                                 appendix_info = copy.deepcopy(part_info)
                                 appendix_info["TEXT"] = ""
-                                
+
                                 # check if appendix is null
                                 if not appendix.find("EAR"): continue
-                                appendix_info["APPENDIX NO"] = appendix.find("EAR").text
-                                appendix_info["APPENDIX SUBJECT"] = appendix.find("HD").text
-                                
+                                appendix_info["APPENDIX NO"] = appendix.find("EAR").text # yapf:disable
+                                appendix_info["APPENDIX SUBJECT"] = appendix.find("HD").text # yapf:disable
+
                                 for child in appendix:
                                     if child.tag == "P":
-                                        appendix_info["TEXT"] += ''.join(child.itertext()).strip() 
-                                        
+                                        appendix_info["TEXT"] += ''.join(child.itertext()).strip() # yapf:disable
+
                                 if appendix_info["TEXT"] != "":
-                                    data = data.append(appendix_info, ignore_index=True)
+                                    data = data.append(appendix_info,
+                                                       ignore_index=True)
 
                         # pull section data not in subparts
                         for section in part.iter("SECTION"):
                             sect_info = copy.deepcopy(part_info)
                             sect_info["TEXT"] = ""
-                            
+
                             # check if section is null
                             if not section.find("P"): continue
-                            
+
                             for child in section:
                                 if child.tag == "SECTNO": 
-                                    sect_info["SECTNO"] = child.text.split()[-1]
-                                elif child.tag == "SUBJECT": 
+                                    sect_info["SECTNO"] = child.text.split()[-1] # yapf:disable
+                                elif child.tag == "SUBJECT":
                                     sect_info["SECT SUBJECT"] = child.text
                                 elif child.tag == "P":
-                                    sect_info["TEXT"] += ''.join(child.itertext()).strip()  
-                            
+                                    sect_info["TEXT"] += ''.join(child.itertext()).strip() # yapf:disable
+
                             # check if section already added while iterating through subparts
                             # if not, add here
-                            idx = data[(data["SECTNO"] == sect_info["SECTNO"]) & 
-                                        (data["TITLE"] == sect_info["TITLE"]) &
-                                        (data["DATE"] == sect_info["DATE"])].index
+                            idx = data[(data["SECTNO"] == sect_info["SECTNO"])
+                                       & (data["TITLE"] == sect_info["TITLE"])
+                                       & (data["DATE"] == sect_info["DATE"])].index # yapf:disable
 
                             if len(idx) == 0:
-                                data = data.append(sect_info, ignore_index=True)
-                                
+                                data = data.append(sect_info,
+                                                   ignore_index=True)
+
                         # pull appendix data not in subparts
                         for appendix in part.iter("APPENDIX"):
-                                
+
                             appendix_info = copy.deepcopy(part_info)
                             appendix_info["TEXT"] = ""
 
                             # check if appendix is null
                             if not appendix.find("EAR"): continue
-                            appendix_info["APPENDIX NO"] = appendix.find("EAR").text
-                            appendix_info["APPENDIX SUBJECT"] = appendix.find("HD").text
+                            appendix_info["APPENDIX NO"] = appendix.find("EAR").text # yapf:disable
+                            appendix_info["APPENDIX SUBJECT"] = appendix.find("HD").text # yapf:disable
 
                             for child in appendix:
                                 if child.tag == "P":
-                                    appendix_info["TEXT"] += ''.join(child.itertext()).strip() 
-                                    
-                            idx = data[(data["APPENDIX NO"] == appendix_info["APPENDIX NO"]) & 
-                                        (data["TITLE"] == appendix_info["TITLE"]) &
-                                        (data["DATE"] == appendix_info["DATE"])].index
+                                    appendix_info["TEXT"] += ''.join(child.itertext()).strip() # yapf:disable
+
+                            idx = data[
+                                (data["APPENDIX NO"] == appendix_info["APPENDIX NO"]) & 
+                                (data["TITLE"] == appendix_info["TITLE"]) &
+                                (data["DATE"] == appendix_info["DATE"])].index # yapf:disable
 
                             if len(idx) == 0 and appendix_info["TEXT"] != "":
-                                data = data.append(appendix_info, ignore_index=True)
+                                data = data.append(appendix_info,
+                                                   ignore_index=True)
 
         return data, parts
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG,
